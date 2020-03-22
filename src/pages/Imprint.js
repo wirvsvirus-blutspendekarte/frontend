6d91eaa0206639adb522918273e78f3d64bbe186
Leo Herrmann <leo.herrmann@physik.tu-berlin.de>
2020-03-23 00:07:47 +0100

    Link gefixt

--- src/pages/Imprint.js
+++ src/pages/Imprint.js
@@ -8,7 +8,7 @@ class Privacy extends React.Component {
           Im Rahmen des Hackathons #wirvsvirus haben Mitglieder des <a href="https://www.juforum.de">deutschen Jungforschernetzwerks – juFORUM e.V.</a> diese Webseite erstellt.
 		  <h2>Verein</h2>
 			Das <a href="https://www.juforum.de">Deutsche Jungforschernetzwerk – juFORUM e.V.</a> wird als eingetragener Verein unter der Aktennummer VR1889 im Vereinsregister des Amtsgerichts Offenbach am Main geführt. Als Förderer von Wissenschaft und Forschung sowie der Volks- und Berufsbildung sowie der Studentenhilfe ist juFORUM durch Bescheinigung des Finanzamtes Köln-Porz vom 15.10.2017 mit der Steuernummer 216/5737/1967 gemäß §5 Abs. 1 Nr. 9 des Körperschaftssteuergesetzes als ausschließlich und unmittelbar gemeinnützigen Zwecken dienend anerkannt worden. Die aktuelle Vereinsadresse lautet: Zu den Wiesen 12, 51147 Köln. Die Vorstandsmitglieder sind zur Zeit: Niklas Wendel, Louisa Gerhard, Matthias Klimpel, Lara Grabitz und Hannes Riechert.
-			Der Vorstand ist erreichbar unter <a href="mailto:vorstand@juforum.de">vorstand@juforum.de</a>. Für weitere Kontaktinformationen sei auf <a href="https://www.juforum.de">die entsprechende Seite www.juforum.de</a> verwiesen.
+			Der Vorstand ist erreichbar unter <a href="mailto:vorstand@juforum.de">vorstand@juforum.de</a>. Für weitere Kontaktinformationen sei auf <a href="https://www.juforum.de/kontakt">die entsprechende Seite www.juforum.de</a> verwiesen.
 		  <h2>Verantwortlichkeit</h2>
 			Verantwortlich für Inhalt, Konzeption und Design dieser Website zeichnet ausschließlich das <a href="https://www.juforum.de">Deutsche Jungforschernetzwerk – juFORUM e.V.</a>, vertreten durch den 1. Vorsitzenden Niklas Wendel, Zu den Wiesen 12, 51147 Köln.
 		  <h2>Lizenz des Codes</h2>
