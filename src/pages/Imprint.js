ed949d86be415c61232d57844d69ebe18c83422a
Leo Herrmann <leo.herrmann@physik.tu-berlin.de>
2020-03-23 00:05:59 +0100

    Impressum erstellt

--- src/pages/Imprint.js
+++ src/pages/Imprint.js
@@ -5,6 +5,18 @@ class Privacy extends React.Component {
     return (
       <div className={"text-content"}>
         <h1>Impressum</h1>
+          Im Rahmen des Hackathons #wirvsvirus haben Mitglieder des <a href="https://www.juforum.de">deutschen Jungforschernetzwerks – juFORUM e.V.</a> diese Webseite erstellt.
+		  <h2>Verein</h2>
+			Das <a href="https://www.juforum.de">Deutsche Jungforschernetzwerk – juFORUM e.V.</a> wird als eingetragener Verein unter der Aktennummer VR1889 im Vereinsregister des Amtsgerichts Offenbach am Main geführt. Als Förderer von Wissenschaft und Forschung sowie der Volks- und Berufsbildung sowie der Studentenhilfe ist juFORUM durch Bescheinigung des Finanzamtes Köln-Porz vom 15.10.2017 mit der Steuernummer 216/5737/1967 gemäß §5 Abs. 1 Nr. 9 des Körperschaftssteuergesetzes als ausschließlich und unmittelbar gemeinnützigen Zwecken dienend anerkannt worden. Die aktuelle Vereinsadresse lautet: Zu den Wiesen 12, 51147 Köln. Die Vorstandsmitglieder sind zur Zeit: Niklas Wendel, Louisa Gerhard, Matthias Klimpel, Lara Grabitz und Hannes Riechert.
+			Der Vorstand ist erreichbar unter <a href="mailto:vorstand@juforum.de">vorstand@juforum.de</a>. Für weitere Kontaktinformationen sei auf <a href="https://www.juforum.de">die entsprechende Seite www.juforum.de</a> verwiesen.
+		  <h2>Verantwortlichkeit</h2>
+			Verantwortlich für Inhalt, Konzeption und Design dieser Website zeichnet ausschließlich das <a href="https://www.juforum.de">Deutsche Jungforschernetzwerk – juFORUM e.V.</a>, vertreten durch den 1. Vorsitzenden Niklas Wendel, Zu den Wiesen 12, 51147 Köln.
+		  <h2>Lizenz des Codes</h2>
+			Organisation: <a href="https://www.juforum.de">Deutsches Jungforschernetzwerk – juFORUM e.V.</a>
+			License: <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0, https://creativecommons.org/licenses/by-nc-sa/4.0/</a>
+			Wenn Sie Interesse haben, an diesem Projekt / dem dazugehörigen Code mitzuarbeiten, würden wir uns sehr freuen, wenn Sie uns diesbezüglich informieren und Ihre Arbeit diesbezüglich mit uns teilen. <a href="https://github.com/wirvsvirus-blutspendekarte">Wir freuen uns über pull requests an: https://github.com/wirvsvirus-blutspendekarte</a>
+		  <h2>Garantie</h2>
+			Diese Website wurde und wird vom <a href="https://www.juforum.de">Deutschen Jungforschernetzwerk – juFORUM e.V.</a> mit größtmöglicher Sorgfalt und nach bestem Wissen und Gewissen erstellt. Trotzdem kann keine Fehlerfreiheit garantiert werden. Jegliche Haftung für Schäden, die aus der Benutzung dieser Website entstehen, wird ausgeschlossen.
       </div>
     )
   }
